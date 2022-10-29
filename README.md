cb7edd5d5712e44a8fcb47913c59a588f945ae0c
Jesus Perez <jasp402@gmail.com>
2022-10-29 12:47:59 -0500

    feat: minor fixes

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ This an open source project that celebrates internal and external contributions.
 **Tailwind Users Dashboard** https://tailwindcomponents.com/component/tailwind-users-dashboard
 <br>
 **electron-puppeteer** https://github.com/luanpotter/electron-puppeteer
+
+
+If you're looking for a way to download images and videos from Instagram profiles, then you need the Instagram Image and Video Downloader app. This app allows you to create a list of profiles to download from, and it will download them one by one. You can also log in to Instagram
+
+
+This software is designed to help you download images and videos from Instagram profiles that you are following. You can create a list of profiles to download from, and the software will download them one by one. You can also log in to Instagram to access your friends' accounts that are not public.
+
+
+
+
+1. Save time: With this app, you can save time by downloading images and videos from multiple Instagram profiles at once.
+
+2. Organize your downloads: This app lets you organize your downloads into separate lists so you can easily find them later.
+
+3. Download private
--- renderer.js
+++ renderer.js
@@ -1,57 +1,52 @@
 const { open } = require('./puppeteer.js');
 const ipcRenderer = require('electron').ipcRenderer;
-const templateAccountCard = (...arg) => `<div
-                    class="mt-8 flex px-4 py-4 justify-between bg-whitedark:bg-gray-600 shadow-xl rounded-lg cursor-pointer">
-                <!-- Card -->
+const templateAccountCard = (...arg) => `
+<div class="mt-8 flex px-4 py-4 justify-between bg-whitedark:bg-gray-600 shadow-xl rounded-lg cursor-pointer">
+    <!-- Card -->
+    <div class="flex justify-between">
+        <!-- Left side -->
+        <img id="profileImg"
+                class="h-12 w-12 rounded-full object-cover"
+                src="./assets/img/profile_empty.jpg"
+                alt="" />
+        <div class="ml-4 flex flex-col capitalize text-gray-600dark:text-gray-400">
+            <span>Account</span>
+            <span class="mt-2 text-black dark:text-gray-200">${arg[0]}</span>
+        </div>
 
-                <div class="flex justify-between">
-                    <!-- Left side -->
+    </div>
+    <div class="flex">
+        <!-- Rigt side -->
 
-                    <img
-                            class="h-12 w-12 rounded-full object-cover"
-                            src="https://appzzang.me/data/editor/1608/f9c387cb6bd7a0b004f975cd92cbe2d9_1471626325_6802.png"
-                            alt="" />
+        <div
+                class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
+            <span>Posts</span>
+            <span class="mt-2 text-black dark:text-gray-200" id="${String(arg[0]).replace(/\./g, '_')}-posts">0</span>
+        </div>
 
-                    <div
-                            class="ml-4 flex flex-col capitalize text-gray-600dark:text-gray-400">
-                        <span>Account</span>
-                        <span class="mt-2 text-black dark:text-gray-200">${arg[0]}</span>
-                    </div>
+        <div
+                class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
+            <span>Type</span>
+            <span class="mt-2 text-black dark:text-gray-200">inaccessible</span>
+        </div>
 
-                </div>
+        <div
+                class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
+            <span>status</span>
+            <span class="mt-2 text-yellow-600 dark:text-yellow-400">Ready to start</span>
+        </div>
 
-                <div class="flex">
-                    <!-- Rigt side -->
+        <div
+                class="mr-8 flex flex-col capitalize text-gray-600dark:text-gray-400">
+            <span>final date</span>
+            <span class="mt-2 text-green-400 dark:text-green-200">---</span>
+        </div>
 
-                    <div
-                            class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
-                        <span>Posts</span>
-                        <span class="mt-2 text-black dark:text-gray-200" id="${String(arg[0]).replace(/\./g,'_')}-posts">0</span>
-                    </div>
-
-                    <div
-                            class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
-                        <span>Type</span>
-                        <span class="mt-2 text-black dark:text-gray-200">inaccessible</span>
-                    </div>
-
-                    <div
-                            class="mr-16 flex flex-col capitalize text-gray-600dark:text-gray-400">
-                        <span>status</span>
-                        <span class="mt-2 text-yellow-600 dark:text-yellow-400">Ready to start</span>
-                    </div>
-
-                    <div
-                            class="mr-8 flex flex-col capitalize text-gray-600dark:text-gray-400">
-                        <span>final date</span>
-                        <span class="mt-2 text-green-400 dark:text-green-200">---</span>
-                    </div>
-
-                </div>
-
-            </div>`;
+    </div>
+</div>
+`;
 let wrapCard = document.getElementById('wrapCard');
-let dir = undefined;
+let dir = 'profiles';
 let user = undefined;
 let pass = undefined;
 let accounts = undefined;
@@ -65,6 +60,12 @@ ipcRenderer.on('variable-reply', function (event, args) {
     dir = args;
     let path  = document.getElementById('path');
     path.innerText = args;
+});
+
+document.getElementById('formLogin').addEventListener('submit', (event) => {
+    event.preventDefault();
+    user = document.getElementById('login');
+    pass = document.getElementById('password');
 });
 
 document.querySelector('#setAccount').addEventListener('click', async () => {
@@ -89,7 +90,5 @@ document.querySelector('#setAccount').addEventListener('click', async () => {
 });
 
 document.querySelector('#startDownload').addEventListener('click', async () => {
-    user = await document.getElementById('login');
-    pass = await document.getElementById('password');
     await open(user.value, pass.value, accounts.join(','), dir);
 });
